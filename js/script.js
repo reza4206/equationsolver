aaf463abe456cf50ed64b49a20d6990edf9bd9fc
reza4206 <163668523+reza4206@users.noreply.github.com>
2024-03-16 23:25:41 +0600

    test

--- js/script.js
+++ js/script.js
@@ -3,15 +3,18 @@ function showLuck() {
     var dob = document.getElementById('dob').value;
 
     if (name && dob) {
-        var randomNum = Math.floor(Math.random() * 10) + 1; // Generates random number between 1 and 100
+        var randomNum = Math.floor(Math.random() * 100) + 1; // Generates random number between 1 and 100
 
         // Fetching a random quote from the web (this part can be done using APIs)
         fetch('https://api.quotable.io/random')
             .then(response => response.json())
             .then(data => {
                 var quote = data.content;
-                var resultDiv = document.getElementById('result');
-                resultDiv.innerHTML = "Your Lucky Number is " + randomNum + " <br> '" + quote + "'";
+                var result = "Your Lucky Number is " + randomNum + " <br> '" + quote + "'";
+                document.getElementById('form').style.display = 'none';
+                document.getElementById('result').innerHTML = result;
+                document.getElementById('result').style.display = 'block';
+                document.getElementById('backButton').style.display = 'block';
             })
             .catch(error => {
                 console.error('Error fetching quote:', error);
@@ -24,5 +27,10 @@ function showLuck() {
 function clearFields() {
     document.getElementById('name').value = '';
     document.getElementById('dob').value = '';
-    document.getElementById('result').innerHTML = '';
+}
+
+function goBack() {
+    document.getElementById('form').style.display = 'block';
+    document.getElementById('result').style.display = 'none';
+    document.getElementById('backButton').style.display = 'none';
 }
